405adddf1513cbe737072203582bc09af7abed01
o_omi <whdudal1217@naver.com>
2022-11-20 23:43:12 +0900

    :sparkles: 멤버 서비스 로직 추가

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package hello.hellospring.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemoryMemberRepository repository;
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        repository = new MemoryMemberRepository();
+        memberService = new MemberService(repository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member result = memberService.fineOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(result.getName());
+    }
+
+    @Test
+    void join_duplicate_exception() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+    @Test
+    void fineOne() {
+    }
+}
