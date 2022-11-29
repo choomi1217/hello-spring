eba1c4deca1939ece3fe23d34ea7fc8d711bce7c
o_omi <whdudal1217@naver.com>
2022-11-30 06:05:00 +0900

    :sparkles: join 메소드에 시간측정 로직 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,10 +21,20 @@ public class MemberService {
      * */
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원 x
-        validateDuplicateMember(member);
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("Join = " + timeMs + "ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
