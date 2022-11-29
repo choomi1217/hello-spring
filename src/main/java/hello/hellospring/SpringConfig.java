5f5adc5576c5d8700cf25812eb05c09393983d25
o_omi <whdudal1217@naver.com>
2022-11-29 21:55:38 +0900

    :sparkles: Spring Data Jpa Config

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -9,6 +9,7 @@ import hello.hellospring.service.MemberService;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,23 +21,34 @@ public class SpringConfig {
     public SpringConfig(DataSource dataSource) { this.dataSource = dataSource; }
     */
 
+    /*
     //@PersistenceContext
     private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        //return new JpaMemberRepository(em);
     }
+    */
 }
